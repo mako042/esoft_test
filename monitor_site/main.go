350bd32473802bab89ace9ed8d47232c911487df
mako042 <leoshanw@gmail.com>
2025-09-18 18:16:16 +0500

    Add new folder

---
+++ monitor_site/main.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+type Data struct {
+	time string
+	url  string
+	status string
+	descriptions string 
+}
+
+func main() {
+	//empty
+}
