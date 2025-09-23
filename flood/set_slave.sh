8834bb91b3b0985dff89b2eec1184894178f61f8
mako042 <leoshanw@gmail.com>
2025-09-23 10:25:59 +0500

    Обновил скрипт

--- flood/set_slave.sh
+++ flood/set_slave.sh
@@ -2,15 +2,15 @@
 
 MASTER_USER="replication"
 
-mysql --port 3306 -u root -e "
+mysql -h 127.0.0.1 --port 3306 -u root -e "
 FLUSH TABLES WITH READ LOCK;
 SHOW MASTER STATUS;"
 
-MASTER_STATUS=$(mysql --port 3306 -u root -e "SHOW MASTER STATUS" | grep mysql-bin)
+MASTER_STATUS=$(mysql -h 127.0.0.1 --port 3306 -u root -e "SHOW MASTER STATUS" | grep mysql-bin)
 MASTER_LOG_FILE=$(echo $MASTER_STATUS | awk '{print $1}')
 MASTER_LOG_POS=$(echo $MASTER_STATUS | awk '{print $2}')
 
-mysql --port 3307 -u root -e "
+mysql -h 127.0.0.1 --port 3307 -u root -e "
 STOP SLAVE;
 CHANGE MASTER TO
 MASTER_HOST='mysql-master',
@@ -19,6 +19,6 @@ MASTER_LOG_FILE='$MASTER_LOG_FILE',
 MASTER_LOG_POS=$MASTER_LOG_POS;
 START SLAVE;"
 
-mysql --port 3306 -u root -e 'UNLOCK TABLES;'
+mysql -h 127.0.0.1 --port 3306 -u root -e 'UNLOCK TABLES;'
 
-mysql --port 3307 -u root -e 'SHOW SLAVE STATUS\G'
+mysql -h  127.0.0.1 --port 3307 -u root -e 'SHOW SLAVE STATUS\G'
